b9375a4b0f27a7b8e315420eaf247be3e4dc5a92
deek-74 <153661141+deek-74@users.noreply.github.com>
2024-01-21 10:47:41 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+
+
+function openPopup() {
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+// function redirect(url) {
+//     window.location.href = url;
+// }
+function search(){
+var mybutton = document.getElementsByClassName("top");
+
+top.addEventListener("click", function () {
+    
+    document.documentElement.scrollTop = 0; 
+  });
+}
